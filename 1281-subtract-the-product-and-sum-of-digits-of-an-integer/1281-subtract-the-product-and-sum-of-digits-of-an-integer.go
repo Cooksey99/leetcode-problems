ee464f493968a489787bdde7e68c02171f83f933
Andrew Cooksey <48497087+Cooksey99@users.noreply.github.com>
2022-05-17 18:09:31 -0500

    Time: 2 ms (27.95%), Space: 1.9 MB (86.87%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.go
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.go
@@ -2,10 +2,8 @@ func subtractProductAndSum(n int) int {
     sum := 0
     product := 1
     
-    var temp int
-    
     for n > 0 {
-        temp = n % 10;
+        temp := n % 10;
         sum = sum + temp
         product = product * temp
     
